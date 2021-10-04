72cf55cb9a3718a9246bbc133ec29d182edb0e57
nicoFdez <nicolafe@ucm.es>
2021-10-04 14:06:57 +0200

    Logica de cell ++
    Falta continuar con las pistas, en concreto está a medias la pista 3
    
    Co-Authored-By: Alonefcp <47754529+Alonefcp@users.noreply.github.com>
    Co-Authored-By: FenomenCoyote <47754234+FenomenCoyote@users.noreply.github.com>

--- Practica1/pcohno/src/main/java/es/ucm/gdv/pcohno/Board.java
+++ Practica1/pcohno/src/main/java/es/ucm/gdv/pcohno/Board.java
@@ -9,7 +9,7 @@ public class Board {
         _size = size;
         for (int i = 0; i < size; ++i){
             for (int j = 0; j < size; ++j){
-                _board[i][j] = new Cell(i, j);
+                _board[i][j] = new Cell(i, j, size);
             }
         }
     }
@@ -20,11 +20,25 @@ public class Board {
     public Pair<Integer, Integer> wrongCell(){
         for (int i = 0; i < _size; ++i){
             for (int j = 0; j < _size; ++j){
-                if(!_board[i][j].isRight(_board, _size))
+                if(!_board[i][j].isRight(_board))
                     return new Pair<Integer, Integer>(i, j);
             }
         }
         return new Pair<Integer, Integer>(-1, -1);
+    }
+
+    public void getHint() {
+        Cell hintCell;
+        for (int i = 0; i < _size; ++i){
+            for (int j = 0; j < _size; ++j){
+                hintCell = _board[i][j].getHint();
+                Pair<Integer, Integer> hintPos = hintCell.getPos();
+                if(hintPos.fst != -1 && hintPos.snd != -1) {
+                    _board[hintPos.fst][hintPos.snd] = hintCell;
+                    return;
+                }
+            }
+        }
     }
 
     public int getSize(){ return _size;}
--- Practica1/pcohno/src/main/java/es/ucm/gdv/pcohno/Cell.java
+++ Practica1/pcohno/src/main/java/es/ucm/gdv/pcohno/Cell.java
@@ -4,20 +4,24 @@ import com.sun.tools.javac.util.Pair;
 
 public class Cell {
 
+    private static Pair<Integer, Integer>[] _dirs = { (-1,0),(1,0),(0,-1),(0,1) };
+
     private enum State {Unassigned, Point, Wall}
     private enum Dirs {Up, Down, Left, Right}
 
-    public Cell(int posX, int posY){
+    public Cell(int posX, int posY, int boardSize){
         this._locked = false;
         this._mustWatch = -1;
         this._pos = new Pair<Integer, Integer>(posX, posY);
+        this._boardSize = boardSize;
         this._state = State.Unassigned;
     }
 
-    public Cell(boolean locked, int mustWatch, int posX, int posY, State state){
+    public Cell(boolean locked, int mustWatch, int posX, int posY, int boardSize, State state){
         this._locked = locked;
         this._mustWatch = mustWatch;
         this._pos = new Pair<Integer, Integer>(posX, posY);
+        this._boardSize = boardSize;
         this._state = state;
     }
 
@@ -29,6 +33,8 @@ public class Cell {
         return false;
     }
 
+    public Pair<Integer, Integer> getPos() { return _pos; }
+
     public void setMustWatch(int mustWatch){ _mustWatch = mustWatch; }
 
     public void setLocked(boolean locked){ _locked = locked; }
@@ -37,62 +43,131 @@ public class Cell {
         return _state == State.Point;
     }
 
-   /** devuelve un array de 4 con el numero de puntos que ve en cada direccion :)
-    * public int[] getView() {
-
-    }
-    */
-
-    public boolean isRight(Cell[][] board, int size){
+    public boolean isRight(Cell[][] board){
         if(_state == State.Unassigned)
             return false;
         else if(_state == State.Wall)
             return true;
+        else if (_locked){
+            int seeing = lookDirections(board);
+            return seeing == _mustWatch;
+        }
+        else {
+            int seeing = lookDirections(board);
+            return seeing > 0;
+        }
+    }
 
-        //Look for mistakes
+    //Devuelve un array de 4 con el numero de puntos que ve en cada direccion :)
+    private int[] getView(Cell[][] board) {
+        int[] view = new int[4];
+        view[0] = lookDirection(Dirs.Up, board);
+        view[1] = lookDirection(Dirs.Down, board);
+        view[2] = lookDirection(Dirs.Left, board);
+        view[3] = lookDirection(Dirs.Right, board);
+        return view;
+    }
+
+    private int lookDirections(Cell[][] board){
         int seeing = 0;
 
         //Look up
-        int row = _pos.fst;
-        while(seeing <= _mustWatch && row >= 0){
-            if(board[row][_pos.snd].isPoint()) {
-                ++seeing;
-                --row;
-            } else break;
-        }
-
+        seeing += lookDirection(Dirs.Up, board);
         //Look down
-        row = _pos.fst;
-        while(seeing <= _mustWatch && row < size){
-            if(board[row][_pos.snd].isPoint()){
-                ++seeing;
-                ++row;
-            } else break;
-        }
-
+        seeing+= lookDirection(Dirs.Down, board);
         //Look left
-        int col = _pos.snd;
-        while(seeing <= _mustWatch && col >= 0){
-            if(board[_pos.fst][col].isPoint()){
-                ++seeing;
-                --col;
-            } else break;
-        }
-
+        seeing += lookDirection(Dirs.Left, board);
         //Look right
-        col = _pos.snd;
-        while(seeing <= _mustWatch && col  < size){
-            if(board[_pos.fst][col].isPoint()){
-                ++seeing;
-                ++col;
-            } else break;
+        seeing += lookDirection(Dirs.Right, board);
+
+        return seeing;
+    }
+
+    private int lookDirection(Dirs direction, Cell[][] board){
+        Pair<Integer,Integer> dir = _dirs[direction];
+
+        int row = _pos.fst;
+        int col = _pos.snd;
+        int seeing = 0;
+        while(row >= 0 && row < _boardSize && col >= 0 && col < _boardSize){
+            if(board[row][col].isPoint()) ++seeing;
+            row += dir.fst; col += dir.snd;
+            else break;
+        }
+        return seeing;
+    }
+
+    public Cell getHint(Cell[][] board){
+        //Probamos con pista 1
+        //Si un número tiene ya visibles el número de celdas que dice, entonces se puede cerrar
+        int seeing = lookDirections(board);
+        if(_locked && seeing == _mustWatch){
+            for(Dirs dir : Dirs.values()){
+                int row = _pos.fst + _dirs[dir].fst;
+                int col = _pos.snd + _dirs[dir].snd;
+                boolean emptyPos = false;
+                while(row >= 0 && row < _boardSize && col >= 0 && col < _boardSize && !emptyPos){
+                    if (board[row][col]._state == State.Unassigned) emptyPos = true;
+                    row += _dirs[dir].fst; col += _dirs[dir].snd;
+                }
+                if(emptyPos) return new Cell(false, -1, row, col, _boardSize, State.Wall);
+            }
         }
 
-        return seeing == _mustWatch;
+        //Probamos con pista 2
+        /*Si pusiéramos un punto azul en una celda vacía, superaríamos el número de visibles
+        del número, y por tanto, debe ser una pared*/
+        if(_locked && seeing < _mustWatch){
+            for(Dirs dir : Dirs.values()) {
+                int row = _pos.fst + _dirs[dir].fst;
+                int col = _pos.snd + _dirs[dir].snd;
+                while(row >= 0 && row < _boardSize && col >= 0 && col < _boardSize
+                    && board[row][col]._state != State.Wall){
+                    if(board[row][col]._state == State.Point)
+                        continue;
+
+                    board[row][col]._state = State.Point;
+                    if(lookDirections(board) > _mustWatch)
+                        return new Cell(false, -1, row, col, _boardSize, State.Wall);
+                    else {
+                        board[row][col]._state = State.Unassigned;
+                    }
+                    row += _dirs[dir].fst; col += _dirs[dir].snd;
+                }
+            }
+        }
+
+        //Probamos con pista 3
+        /*Si no ponemos un punto en alguna celda vacía, entonces es imposible alcanzar el
+        número*/
+        if(_locked && seeing < _mustWatch){
+            int[] freePositions = unassignedPositions(board);
+            int numAvailableDirs = 0;
+            Dirs availableDir = Dirs.Up;
+            for(Dirs dir : Dirs.values()){
+                if(freePositions[dir] >= _mustWatch - seeing){
+                    numAvailableDirs++;
+                    availableDir = dir;
+                }
+            }
+            if(numAvailableDirs == 1){
+
+            }
+        }
+
+        private int[] canWatchPositions(Cell[][] board){
+            
+        }
+
+        //Si no he podido usar ninguna pista, devuelvo una cell con posicion invalida
+        return new Cell (false, -1, -1, -1, _boardSize, State.Point);
     }
+
+
 
     private boolean _locked;
     private int _mustWatch;
     private Pair<Integer, Integer> _pos;
     private State _state;
+    private int _boardSize;
 }
