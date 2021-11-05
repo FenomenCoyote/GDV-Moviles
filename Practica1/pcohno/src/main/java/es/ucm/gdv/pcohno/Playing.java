01a543f11f6ec2de380e43e15faed1133612a833
Alonefcp <fcuadra@ucm.es>
2021-11-05 16:53:10 +0100

    Puesto los botones que faltaban (eye y undo) y el texto del porcentaje

--- Practica1/pcohno/src/main/java/es/ucm/gdv/pcohno/Playing.java
+++ Practica1/pcohno/src/main/java/es/ucm/gdv/pcohno/Playing.java
@@ -13,15 +13,23 @@ public class Playing extends State {
         super(graphics, input);
 
         font1 = graphics.newFont("Resources/fonts/JosefinSans-Bold.ttf",64,false);
+        font2 = graphics.newFont("Resources/fonts/JosefinSans-Bold.ttf",16,false);
 
         imgClose = graphics.newImage("Resources/sprites/close.png");
+        imgUnDo = graphics.newImage("Resources/sprites/history.png");
+        imgEye = graphics.newImage("Resources/sprites/eye.png");
 
         clickableClose = new ClickImage(imgClose, 150 - imgClose.getWidth()/2, 1050, 100, 100);
+        clickableUnDo = new ClickImage(imgUnDo, 400 - imgUnDo.getWidth()/2, 1050, 100, 100);
+        clickableEye = new ClickImage(imgEye, 650 - imgEye.getWidth()/2, 1050, 100, 100);
     }
 
     @Override
     public void render() {
+
         graphics.setColor(0xff222222);
+        graphics.setFont(font2);
+        graphics.drawText("0%",200,510);
         graphics.setFont(font1);
         graphics.drawText(boardSize + " x " + boardSize, 200, 80);
 
@@ -31,6 +39,8 @@ public class Playing extends State {
 
         graphics.scale(0.5f, 0.5f);
         clickableClose.render(graphics);
+        clickableUnDo.render(graphics);
+        clickableEye.render(graphics);
     }
 
     @Override
@@ -41,6 +51,14 @@ public class Playing extends State {
             if(clickableClose.isOnMe(t.x * 2, t.y * 2)){
                 events.clear();
                 return OhNoApplication.State.Menu;
+            }
+            else if(clickableEye.isOnMe(t.x * 2, t.y * 2))
+            {
+                events.clear();
+            }
+            else if(clickableUnDo.isOnMe(t.x * 2, t.y * 2))
+            {
+                events.clear();
             }
             board.isOnMe(t.x, t.y);
         }
@@ -58,9 +76,9 @@ public class Playing extends State {
 
     private Board board;
 
-    private MyFont font1;
+    private MyFont font1,font2;
 
-    private Image imgClose;
+    private Image imgClose,imgUnDo,imgEye;
 
-    private ClickImage clickableClose;
+    private ClickImage clickableClose,clickableUnDo, clickableEye;
 }
