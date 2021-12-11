cbbc2eb7368adbceb4d34cbf281d6f6589a8a510
Alonefcp <fcuadra@ucm.es>
2021-12-12 00:37:25 +0100

    Puesto lo que hay que guardar en el juego

--- Practica2/Assets/Scripts/Logic/GameState.cs
+++ Practica2/Assets/Scripts/Logic/GameState.cs
@@ -2,9 +2,28 @@ using System;
 
 namespace flow.Logic
 {
+
+    [Serializable]
+    public struct Level
+    {
+        public bool completedOptimally;
+        public bool completed;
+        public bool locked;
+        public int record;
+    }
+
+    [Serializable]
+    public struct LvlPack
+    {
+        int completedLevels;
+        Level[] levels;
+    }
+
     [Serializable]
     public class GameState
     {
         public uint nHints = 5;
+        public LvlPack[] lvlPacks;
+    
     }
 }
